c35f3e7d8a4f9011d7b115755211188d309ebed9
Olesia Maidan <olesia@ace.date>
2023-11-27 20:49:59 +0200

    add TestClass

---
+++ src/main/java/org/example/TestClass.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import com.github.javafaker.Faker;
+
+
+
+public class TestClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        System.out.println(Faker.instance().address().city());
+    }
+}
